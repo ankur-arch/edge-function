6bdf1d66ccc46c5dfce9e04089f8d6e392566363
ankur datta <dev.ankur.datta@gmail.com>
2023-04-04 14:57:41 +0600

    feat: remove cache from quote

--- src/pages/api/quote.js
+++ src/pages/api/quote.js
@@ -17,7 +17,6 @@ export default async function handler(req, res) {
     status: 200,
     headers: {
       "content-type": "application/json",
-      "cache-control": "public, s-maxage=10, stale-while-revalidate=60",
     },
   });
 }
